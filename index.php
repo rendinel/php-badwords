f81b1b40be03584507441bf56b470a80a199bd69
lucio rendine <luciorendine69@gmail.com>
2021-02-25 17:12:59 +0100

    push

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
-echo "ole";
+
+ echo "Mio programma in php";
+?>
+
+
+<span>panda trucidati dai <b>BUG</b> </span>
+<br>
+<br>
+
+<?php
+
+ $variableContent="Questo é il contenuto di una variabile in php";
+ echo $variableContent;
+
+?>
+
+<br>
+
+<?php
+ $name = "Ted";
+?>
+
+<h1>
+  Ciao mi chiamo <?php echo $name; ?>
+</h1>
+
+<br>
+
+<?php
+
+var_dump($_GET);
+
+
+?>
+
+<br>
+
+<?php
+$str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+echo strlen($str);
 ?>
